1d58d079d823706eca58ba4bccc1e4042084a990
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-12-06 21:02:37 +0100

    Update Readme.md

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar/NavBar';
-// import Calculator from '../components/Calculator/Calculator';
 
 const App = () => (
   <>

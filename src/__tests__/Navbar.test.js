880581219a13b518a543d7670a257d1061ffe212
Shisui <37116322+Shisui6@users.noreply.github.com>
2022-12-08 13:16:38 +0100

    Update Navbar.test.js

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from '../components/Navbar/Navbar';
+import Navbar from '../components/Navbar/Navbar.js';
 
 describe('NavBar', () => {
   it('renders correctly', () => {
